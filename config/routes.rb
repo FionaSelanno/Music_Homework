0106c08f757a1a980c0dc2616cda1c2ed77b218f
“FionaSelanno” <fcs_fiona@hotmail.com>
2016-06-14 22:58:44 +0200

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'artists#index'
   resources :artists
   resources :songs
   # The priority is based upon order of creation: first created -> highest priority.
