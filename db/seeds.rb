382e1058ee1e7688096d5a52c3989df8ead6d56f
“FionaSelanno” <fcs_fiona@hotmail.com>
2016-06-14 23:54:23 +0200

    added songslist in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,25 @@ artists = ["Beyonce", "Adele", "Pharrell", "Anouk"]
 artists.each do |name|
   Artist.create(name:name)
 end
+
+artist_songs={}
+
+artist_songs["Beyonce"] =[
+  ["Run the world", "Some of them men think. They freak this like we do. But no they don't.Make your check come at they neck. Disrespect us no they won't..."],
+  ["Irreplacebale", "Mmmm to the left, to the left. Everything you own in the box to the left. In the closet, thats my stuff.Yes, if I bought it, then please don't touch (don't touch)..."]
+]
+
+artist_songs ["Adele"] = [
+  ["Someone like you", "Old friend, why are you so shy. Ain't like you to hold back. Or hide from the light"],
+  ["Skyfall", "This is the end. Hold your breath and count to ten. Feel the earth move and then. Hear my heart burst again..."]
+]
+
+artist_songs ["Pharrell"] = [
+  ["Happy", "It might seem crazy what I'm about to say. Sunshine she's here, you can take a break. I'm a hot air balloon that could go to space. With the air, like I don't care, baby, by the way..."],
+  ["Freedom", "Hold on to me. Don't let me go. Who cares what they see. Who cares what they know? Your first name is Free. Last name is Dom. Cause you still believe..."]
+]
+
+artist_songs ["Anouk"] = [
+  ["Birds", "Isolated from the outside. Clouds have taken all the light. I have no control. It seems my thoughts wander off. Of the time when I tried to. Live life without you..."],
+  ["Lost", "If roses are meant to be red. And violets to be blue. Why isn't my heart meant for you "]
+]
